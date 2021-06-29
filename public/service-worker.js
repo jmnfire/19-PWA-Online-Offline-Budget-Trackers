79f195ca6f712ad1d69b347529df6d7821b36699
Jovan <jovan.m.nixon@gmail.com>
2021-06-28 21:35:15 -0600

    icons

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,13 +8,7 @@ const FILES_TO_CACHE = [
   "/assets/images/Vue.js-icon.png",
   "/manifest.webmanifest",
   "/favicon.ico",
-  "/assets/images/icons/icon-72x72.png",
-  "/assets/images/icons/icon-96x96.png",
-  "/assets/images/icons/icon-128x128.png",
-  "/assets/images/icons/icon-144x144.png",
-  "/assets/images/icons/icon-152x152.png",
   "/assets/images/icons/icon-192x192.png",
-  "/assets/images/icons/icon-384x384.png",
   "/assets/images/icons/icon-512x512.png",
 ];
 
@@ -37,7 +31,7 @@ self.addEventListener("activate", function(evt) {
   evt.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(
-        keyList.map(key => {
+        keyList.map(key => { 
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
             console.log("Removing old cache data", key);
             return caches.delete(key);
